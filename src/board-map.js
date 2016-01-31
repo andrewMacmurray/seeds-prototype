a2199591220f76055c4e81d68c00d96d039c419c
Andrew <a.macmurray@icloud.com>
2016-01-31 15:12:01 +0000

    refactor into buildMap method

--- src/board-map.js
+++ src/board-map.js
@@ -1,25 +1,26 @@
-var rows = document.getElementsByClassName('row');
-rows = [].slice.call(rows);
-console.log(rows[0].childNodes);
+function buildMap() {
+    var rows = document.getElementsByClassName('row');
+    rows = [].slice.call(rows);
+    var map = [];
 
-var map = [];
-
-rows.forEach(function(row, i) {
-    var items = row.childNodes;
-    items = [].slice.call(items);
-    var itemMap = items.map(function(item, j) {
-        var data = {};
-        if (item.firstChild.className === 'item seed') data.type = 'seed';
-        if (item.firstChild.className === 'item water') data.type = 'water';
-        if (item.firstChild.className === 'item sunshine') data.type = 'sunshine';
-        if (item.firstChild.className === 'item seedling') data.type = 'seedling';
-        data.id = '' + i + j;
-        return data;
+    rows.forEach(function(row, i) {
+        var items = row.childNodes;
+        items = [].slice.call(items);
+        var itemMap = items.map(function(item, j) {
+            var data = {};
+            if (item.firstChild.className === 'item seed') data.type = 'seed';
+            if (item.firstChild.className === 'item water') data.type = 'water';
+            if (item.firstChild.className === 'item sunshine') data.type = 'sunshine';
+            if (item.firstChild.className === 'item seedling') data.type = 'seedling';
+            data.id = '' + i + j;
+            return data;
+        });
+        map.push(itemMap);
+        items.forEach(function(item, j) {
+            item.classList.add('id=' + i + '.' + (j+1));
+        });
     });
-    map.push(itemMap);
-    items.forEach(function(item, j) {
-        item.classList.add('id=' + i + '.' + (j+1));
-    });
-});
+    return map;
+}
 
-console.log(JSON.stringify(map));
+console.log(JSON.stringify(buildMap()));
