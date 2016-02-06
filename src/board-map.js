e4086f3566315e3d271c6ae0d71249a0d0ec1c10
Andrew MacMurray <a.macmurray@icloud.com>
2016-02-06 13:50:59 +0000

    added dragging cursor on tiles

--- src/board-map.js
+++ src/board-map.js
@@ -23,17 +23,38 @@ function buildMap() {
 	return map;
 }
 
+var columns = [].slice.call(document.getElementsByClassName('col-2'));
+columns.forEach(column => {
+	column.addEventListener('mousedown', cursorDown);
+	column.addEventListener('mouseup', cursorUp);
+})
+
+function cursorUp() {
+	columns.forEach(column => {
+		column.classList.remove('mouseDown');
+	})
+}
+
+function cursorDown() {
+	columns.forEach(column => {
+		column.classList.add('mouseDown');
+	})
+}
+
 var waterTotal = 0;
 var waterDragging = false;
 function setDragging() {
-    this.className += ' small';
+    this.classList.add('small');
     waterTotal ++;
     waterDragging = true;
 }
+
 function stopDragging() {
     waterDragging = false;
     waterTotal = 0;
+
 }
+
 function addWater() {
     if (waterDragging === true) {
         this.className += ' small';
@@ -41,10 +62,10 @@ function addWater() {
     }
     if (waterTotal === 5) {
         waterTotal = 0;
-        document.body.style.backgroundColor = '#92CAE3';
+        document.body.style.backgroundColor = '#92C9E3';
         setTimeout(function() {
             document.body.style.backgroundColor = '#FFFCD5';
-        }, 3500);
+        }, 1500);
     }
 }
 
