2b7c0d692bafba2d523eceb16e08fb9126de83cf
Andrew <a.macmurray@icloud.com>
2016-03-08 23:56:40 +0000

    hacky removing item from grid function added

--- src/tiles.js
+++ src/tiles.js
@@ -1,26 +1,43 @@
 "use strict";
 const setTiles = (type, color) => {
-	let waterTotal = 0;
-	let waterDragging = false;
+	let counter = 0;
+	let isDragging = false;
+	let tilesArray = [];
 
 	const setDragging = (e) => {
+		tilesArray.push(e.target);
 		e.target.className += ' small';
-		waterTotal++;
-		waterDragging = true;
+		counter++;
+		isDragging = true;
 	};
 
 	const stopDragging = () => {
-		waterDragging = false;
-		waterTotal = 0;
+		tilesArray.forEach((tile) => {
+			tile.classList.remove('small');
+		});
+		tilesArray = [];
+		console.log(tilesArray);
+		isDragging = false;
+		counter = 0;
 	};
 
 	const addWater = (e) => {
-		if (waterDragging === true) {
-			e.target.className += ' small';
-			waterTotal++;
+		if (isDragging === true) {
+			tilesArray.push(e.target);
+			console.log(tilesArray);
+			e.target.firstChild.className += ' small';
+			counter++;
 		}
-		if (waterTotal === 5) {
-			waterTotal = 0;
+		if (counter === 5) {
+			tilesArray.forEach((tile) => {
+				tile.style.opacity = 0;
+				setTimeout(() => {
+					tile.style.display = 'none';
+				}, 300)
+			});
+			tilesArray = [];
+			counter = 0;
+			isDragging = false;
 			document.body.style.backgroundColor = color;
 			setTimeout(function () {
 				document.body.style.backgroundColor = '#FFFCD5';
@@ -29,6 +46,7 @@ const setTiles = (type, color) => {
 	};
 
 	var waterTiles = document.getElementsByClassName(type);
+	console.log(waterTiles);
 	for (let i = 0; i < waterTiles.length; i++) {
 		let waterNode = waterTiles[i].parentNode;
 		waterNode.addEventListener('mousedown', setDragging);
