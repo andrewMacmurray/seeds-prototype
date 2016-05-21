7ce162c78971e55f1d41282673fa7e145c15a592
Andrew MacMurray <a.macmurray@icloud.com>
2016-05-21 23:23:22 +0100

    broke weather reducer, fixed now

--- src/js/reducers/reducer_weatherPower.js
+++ src/js/reducers/reducer_weatherPower.js
@@ -6,15 +6,17 @@ export default (state = defaultState, action) => {
   switch (action.type) {
     case WEATHER_POWER:
 
-      const { weatherType, power } = action.payload.weatherType
+      const { weatherType, power } = action.payload
 
       if (weatherType === 'sun') return { ...state, sun: state.sun + power }
       if (weatherType === 'rain') return { ...state, rain: state.rain + power }
 
     case RESET_WEATHER:
 
-      if (weatherType === 'sun') return { ...state, sun: 0 }
-      if (weatherType === 'rain') return { ...state, rain: 0 }
+      const _weatherType = action.payload
+
+      if (_weatherType === 'sun') return { ...state, sun: 0 }
+      if (_weatherType === 'rain') return { ...state, rain: 0 }
 
     default:
       return state
