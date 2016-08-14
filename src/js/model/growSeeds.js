388866cdd60f31d81fcd2a588b533f72f3b5c205
Andrew MacMurray <a.macmurray@icloud.com>
2016-08-14 23:08:34 +0100

    minor cleanup of growSeeds

--- src/js/model/growSeeds.js
+++ src/js/model/growSeeds.js
@@ -1,16 +1,18 @@
-import { addIndex, filter, reduce, map, concat, compose } from 'ramda'
+import { addIndex, filter, map, unnest, compose } from 'ramda'
 
-const growSeedMove = (tile, i, j) => tile === 3 && Math.random() >= 0.5 ? [ i, j ] : 0
+const growSeedMove = (_, tile, i, j) => tile === 3 && Math.random() >= 0.5 ? [ i, j ] : 0
 const mapWithIndex = addIndex(map)
-const growMovesOnBoard = (board) =>
+
+const mapBoard = (fn) =>
   mapWithIndex((row, i) =>
   mapWithIndex((tile, j) =>
-  growSeedMove(tile, i, j)
-)(row))(board)
+  fn(row, tile, i, j)
+)(row))
 
-const collapseArr = reduce(concat, [])
+const growMovesOnBoard = mapBoard(growSeedMove)
+
 const filterForTiles = filter(x => x !== 0)
-export const growingMoveArray = compose(filterForTiles, collapseArr, growMovesOnBoard)
+export const growingMoveArray = compose(filterForTiles, unnest, growMovesOnBoard)
 
 export const isGrowingArray = (moves, board) =>
   mapWithIndex((row, i) =>
